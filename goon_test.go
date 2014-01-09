cdb6036afba6b2c0d3f64bea42d399c0e96cff74
Matthew Zimmerman <mzimmerman@gmail.com>
2014-01-09 11:43:07 -0500

    adding failing concurrency test

--- goon_test.go
+++ goon_test.go
@@ -231,4 +231,33 @@ func TestPutGet(t *testing.T) {
 		t.Fatal("goonPutGet.Value should be 15 but is",
 			goonPutGet.Value)
 	}
+
+	// Race condition in GetMulti
+	// First, put our race victim
+	// Since no memcache is written on Put, Race does not exist in memcache until Get puts it there
+	// Since there's nothing inside Goon to synchronize on
+	fetchMe := &HasString{Id: "Race"}
+	_, err = g.Put(fetchMe)
+	if err != nil {
+		t.Fatalf("Error putting Race object - %v", err)
+	}
+	errc := make(chan error)
+	f := func(g *Goon) {
+		errc <- g.Get(fetchMe)
+	}
+	failTestNum := 2
+	for i := 0; i < failTestNum; i++ {
+		tg := FromContext(c)
+		tg.testing = true
+		go f(tg)
+	}
+	errored := false
+	for i := 0; i < failTestNum; i++ {
+		if <-errc != nil {
+			errored = true
+		}
+	}
+	if !errored {
+		t.Fatal("Concurrent Get requests should fail due to entry already being in the memcache")
+	}
 }
