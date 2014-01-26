maddyblue/goon

commit a0fb89e8d950f972170506508c2bbadf815c0708

Doing a Get request on an incomplete key is what results in the datastore error "datastore: internal error: server returned the wrong number of entities"  Having goon check for these error conditions saves an RPC request.
getStructKey needs to return an incomplete key for Put requests, so instead added a parameter to extractKeys in order to determine if incomplete keys were okay or not.

Fixed #8, getStructKey(src) throws an error if it's a String ID object and the Id is not populated.  Since an incomplete key Put to the datastore results in an int64 Id, having a
String Id be blank is never helpful.

added more unit tests which is where these errors were caught

Conflicts:
	goon.go
	goon_test.go

goon.go
@@ -66,7 +66,7 @@ func (g *Goon) error(err error) {
 	}
 }
 
-func (g *Goon) extractKeys(src interface{}) ([]*datastore.Key, error) {
+func (g *Goon) extractKeys(src interface{}, allowIncomplete bool) ([]*datastore.Key, error) {
 	v := reflect.Indirect(reflect.ValueOf(src))
 	if v.Kind() != reflect.Slice {
 		return nil, errors.New("goon: value must be a slice or pointer-to-slice")
@@ -80,6 +80,9 @@ func (g *Goon) extractKeys(src interface{}) ([]*datastore.Key, error) {
 		if err != nil {
 			return nil, err
 		}
+		if !allowIncomplete && key.Incomplete() {
+			return nil, fmt.Errorf("goon: cannot find a key for struct - %v", vi.Interface())
+		}
 		keys[i] = key
 	}
 	return keys, nil
@@ -88,7 +91,9 @@ func (g *Goon) extractKeys(src interface{}) ([]*datastore.Key, error) {
 // Key is the same as KeyError, except nil is returned on error.
 func (g *Goon) Key(src interface{}) *datastore.Key {
 	if k, err := g.KeyError(src); err == nil {
-		return k
+		if !k.Incomplete() {
+			return k
+		}
 	}
 	return nil
 }
@@ -155,7 +160,7 @@ const putMultiLimit = 500
 //
 // src must satisfy the same conditions as the dst argument to GetMulti.
 func (g *Goon) PutMulti(src interface{}) ([]*datastore.Key, error) {
-	keys, err := g.extractKeys(src)
+	keys, err := g.extractKeys(src, true) // allow incomplete keys on a Put request
 	if err != nil {
 		return nil, err
 	}
@@ -220,16 +225,9 @@ func (g *Goon) PutComplete(src interface{}) (*datastore.Key, error) {
 
 // PutMultiComplete is like PutMulti, but errors if a key is incomplete.
 func (g *Goon) PutMultiComplete(src interface{}) ([]*datastore.Key, error) {
-	keys, err := g.extractKeys(src)
+	_, err := g.extractKeys(src, false)
 	if err != nil {
 		return nil, err
-	}
-	for i, k := range keys {
-		if k.Incomplete() {
-			err := fmt.Errorf("goon: incomplete key (%dth index): %v", i, k)
-			g.error(err)
-			return nil, err
-		}
 	}
 	return g.PutMulti(src)
 }
@@ -301,7 +299,7 @@ const getMultiLimit = 1000
 //
 // dst has similar constraints as datastore.GetMulti.
 func (g *Goon) GetMulti(dst interface{}) error {
-	keys, err := g.extractKeys(dst)
+	keys, err := g.extractKeys(dst, false) // don't allow incomplete keys on a Get request
 	if err != nil {
 		return err
 	}

goon_test.go
@@ -14,17 +14,14 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-package goon_test
+package goon
 
 import (
 	"testing"
 
-	"appengine"
 	"appengine/aetest"
 	"appengine/datastore"
 	"appengine/memcache"
-
-	"github.com/mjibson/goon"
 )
 
 func TestGoon(t *testing.T) {
@@ -33,12 +30,15 @@ func TestGoon(t *testing.T) {
 		t.Fatalf("Could not start aetest - %v", err)
 	}
 	defer c.Close()
-	n := goon.FromContext(c)
+	n := FromContext(c)
 
 	// key tests
 	noid := NoId{}
-	if k, err := n.KeyError(noid); err != nil || !k.Incomplete() {
-		t.Errorf("expected incomplete on noid")
+	if k, err := n.KeyError(noid); err == nil && !k.Incomplete() {
+		t.Error("expected incomplete on noid")
+	}
+	if n.Key(noid) != nil {
+		t.Error("expected to not find a key")
 	}
 
 	var keyTests = []keyTest{
@@ -50,6 +50,7 @@ func TestGoon(t *testing.T) {
 			HasKind{Id: 1, Kind: "OtherKind"},
 			datastore.NewKey(c, "OtherKind", "", 1, nil),
 		},
+
 		{
 			HasDefaultKind{Id: 1, Kind: "OtherKind"},
 			datastore.NewKey(c, "OtherKind", "", 1, nil),
@@ -57,6 +58,10 @@ func TestGoon(t *testing.T) {
 		{
 			HasDefaultKind{Id: 1},
 			datastore.NewKey(c, "DefaultKind", "", 1, nil),
+		},
+		keyTest{
+			HasString{Id: "new"},
+			datastore.NewKey(c, "HasString", "new", 0, nil),
 		},
 	}
 
@@ -73,10 +78,11 @@ func TestGoon(t *testing.T) {
 	}
 
 	// datastore tests
-
-	initTest(c)
+	keys, _ := datastore.NewQuery("HasId").KeysOnly().GetAll(c, nil)
+	datastore.DeleteMulti(c, keys)
+	memcache.Flush(c)
 	if err := n.Get(&HasId{Id: 0}); err == nil {
-		t.Errorf("ds: expected error")
+		t.Errorf("ds: expected error, we're fetching from the datastore on an incomplete key!")
 	}
 	if err := n.Get(&HasId{Id: 1}); err != datastore.ErrNoSuchEntity {
 		t.Errorf("ds: expected no such entity")
@@ -99,11 +105,11 @@ func TestGoon(t *testing.T) {
 	}
 	if err := n.GetMulti(es); err == nil {
 		t.Errorf("ds: expected error")
-	} else if !goon.NotFound(err, 0) {
+	} else if !NotFound(err, 0) {
 		t.Errorf("ds: not found error 0")
-	} else if !goon.NotFound(err, 1) {
+	} else if !NotFound(err, 1) {
 		t.Errorf("ds: not found error 1")
-	} else if goon.NotFound(err, 2) {
+	} else if NotFound(err, 2) {
 		t.Errorf("ds: not found error 2")
 	}
 	if keys, err := n.PutMulti(es); err != nil {
@@ -149,12 +155,67 @@ func TestGoon(t *testing.T) {
 	if _, err := n.PutComplete(&HasId{Id: 1}); err != nil {
 		t.Errorf("put complete: unexpected error")
 	}
-}
 
-func initTest(c appengine.Context) {
-	keys, _ := datastore.NewQuery("HasId").KeysOnly().GetAll(c, nil)
-	datastore.DeleteMulti(c, keys)
-	memcache.Flush(c)
+	// put a HasId resource, then test pulling it from memory, memcache, and datastore
+	hi := &HasId{Name: "hasid"} // no id given, should be automatically created by the datastore
+	if _, err := n.Put(hi); err != nil {
+		t.Errorf("put: unexpected error - %v", err)
+	}
+	if n.Key(hi) == nil {
+		t.Errorf("key should not be nil")
+	} else if n.Key(hi).Incomplete() {
+		t.Errorf("key should not be incomplete")
+	}
+
+	hi2 := &HasId{Id: hi.Id}
+	if err := n.Get(hi2); err != nil {
+		t.Errorf("get: unexpected error - %v", err)
+	}
+	if hi2.Name != hi.Name {
+		t.Errorf("Could not fetch HasId object from memory - %#v != %#v, memory=%#v", hi, hi2, n.cache[memkey(n.Key(hi2))])
+	}
+
+	hi3 := &HasId{Id: hi.Id}
+	delete(n.cache, memkey(n.Key(hi)))
+	if err := n.Get(hi3); err != nil {
+		t.Errorf("get: unexpected error - %v", err)
+	}
+	if hi3.Name != hi.Name {
+		t.Errorf("Could not fetch HasId object from memory - %#v != %#v", hi, hi3)
+	}
+
+	hi4 := &HasId{Id: hi.Id}
+	delete(n.cache, memkey(n.Key(hi4)))
+	if memcache.Flush(n.context) != nil {
+		t.Errorf("Unable to flush memcache")
+	}
+	if err := n.Get(hi4); err != nil {
+		t.Errorf("get: unexpected error - %v", err)
+	}
+	if hi4.Name != hi.Name {
+		t.Errorf("Could not fetch HasId object from datastore- %#v != %#v", hi, hi4)
+	}
+
+	// Since the datastore can't assign a key to a String ID, test to make sure goon stops it from happening
+	hasString := new(HasString)
+	_, err = n.PutComplete(hasString)
+	if err == nil {
+		t.Errorf("Cannot put an incomplete object using PutComplete - %v", hasString)
+	}
+	_, err = n.Put(hasString)
+	if err == nil {
+		t.Errorf("Cannot put an incomplete string Id object as the datastore will populate an int64 id instead- %v", hasString)
+	}
+	hasString.Id = "hello"
+	_, err = n.PutComplete(hasString)
+	if err != nil {
+		t.Errorf("Error putting hasString object - %v", hasString)
+	}
+	_, err = n.Put(hasString)
+	if err != nil {
+		t.Errorf("Error putting hasString object - %v", hasString)
+	}
+
 }
 
 type keyTest struct {
@@ -182,6 +243,10 @@ type HasDefaultKind struct {
 	Name string
 }
 
+type HasString struct {
+	Id string `datastore:"-" goon:"id"`
+}
+
 type TwoId struct {
 	IntId    int64  `goon:"id"`
 	StringId string `goon:"id"`
@@ -201,7 +266,7 @@ func TestRace(t *testing.T) {
 		t.Fatalf("Could not start aetest - %v", err)
 	}
 	defer c.Close()
-	g := goon.FromContext(c)
+	g := FromContext(c)
 
 	hasid := &HasId{Id: 1, Name: "Race"}
 	_, err = g.Put(hasid)
@@ -221,7 +286,7 @@ func TestPutGet(t *testing.T) {
 		t.Fatalf("Could not start aetest - %v", err)
 	}
 	defer c.Close()
-	g := goon.FromContext(c)
+	g := FromContext(c)
 
 	key, err := g.Put(&PutGet{ID: 12, Value: 15})
 	if err != nil {

commit 6157098c98674cb41ee3434a77a1a01a281c294e

Add a Kind helper function
This is important because the recent change in Key in
a0fb89e8d950f972170506508c2bbadf815c0708 broke incomplete keys. So things
like g.Key(&User{}).Kind() would panic with a nil reference error.

goon.go
@@ -111,6 +111,14 @@ func (g *Goon) Key(src interface{}) *datastore.Key {
 	return nil
 }
 
+// Kind returns src's datastore Kind or "" on error.
+func (g *Goon) Kind(src interface{}) string {
+	if k, err := g.KeyError(src); err == nil {
+		return k.Kind()
+	}
+	return ""
+}
+
 // KeyError returns the key of src based on its properties.
 func (g *Goon) KeyError(src interface{}) (*datastore.Key, error) {
 	return g.getStructKey(src)